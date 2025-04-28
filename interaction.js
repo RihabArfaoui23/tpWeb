aa11ef911a4a9c519faa95d7c66ede042b8890c6
saifbahri <saifbahri06@gmail.com>
2025-04-28 21:27:39 +0200

    tpVanillaJS

--- interaction.js
+++ interaction.js
@@ -1,23 +1,104 @@
-
-// La création d'un Dnd requière un canvas et un interacteur.
-// L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
 function DnD(canvas, interactor) {
-	// Définir ici les attributs de la 'classe'
+    // Attributs pour les positions du DnD
+    this.xDebutDND = 0;
+    this.yDebutDND = 0;
+    this.xFinDND = 0;
+    this.yFinDND = 0;
 
-	// Developper les 3 fonctions gérant les événements
+    // Attribut pour vérifier si une pression a eu lieu
+    this.isPressed = false;
 
-	// Associer les fonctions précédentes aux évènements du canvas.
+    // Fonction pour la gestion de l'événement 'mousedown' (pression de la souris)
+    this.mouseDown = function(evt) {
+        // Obtenir la position de la souris par rapport au canvas
+        var mousePos = getMousePosition(canvas, evt);
+
+        // Initialiser la position de départ du DnD
+        this.xDebutDND = mousePos.x;
+        this.yDebutDND = mousePos.y;
+
+        // Marquer que la pression a eu lieu
+        this.isPressed = true;
+
+        // Afficher dans la console les coordonnées de l'événement
+        console.log('Mouse down at:', this.xDebutDND, this.yDebutDND);
+
+        // Commencer à écouter le mouvement de la souris (mousemove) et le relâchement (mouseup)
+        canvas.addEventListener('mousemove', this.mouseMove.bind(this));
+        canvas.addEventListener('mouseup', this.mouseUp.bind(this));
+    };
+
+    // Fonction pour la gestion de l'événement 'mouseup' (relâchement de la souris)
+    this.mouseUp = function(evt) {
+        if (this.isPressed) {
+            // Obtenir la position de la souris lors du relâchement
+            var mousePos = getMousePosition(canvas, evt);
+
+            // Sauvegarder la position de fin du DnD
+            this.xFinDND = mousePos.x;
+            this.yFinDND = mousePos.y;
+
+            // Afficher dans la console les coordonnées de l'événement
+            console.log('Mouse up at:', this.xFinDND, this.yFinDND);
+
+            // Terminer l'interaction, on peut ici appeler une fonction pour dessiner une forme (ex. rectangle)
+            this.drawShape();
+
+            // Retirer les écouteurs d'événements 'mousemove' et 'mouseup' après avoir terminé l'interaction
+            canvas.removeEventListener('mousemove', this.mouseMove.bind(this));
+            canvas.removeEventListener('mouseup', this.mouseUp.bind(this));
+
+            // Réinitialiser l'état de pression
+            this.isPressed = false;
+        }
+    };
+
+    // Fonction pour la gestion de l'événement 'mousemove' (déplacement de la souris)
+    this.mouseMove = function(evt) {
+        if (this.isPressed) {
+            // Mettre à jour la position finale lors du déplacement
+            var mousePos = getMousePosition(canvas, evt);
+            this.xFinDND = mousePos.x;
+            this.yFinDND = mousePos.y;
+
+            // Afficher dans la console les coordonnées de l'événement
+            console.log('Mouse move at:', this.xFinDND, this.yFinDND);
+
+            // Facultatif : Vous pouvez ici dessiner un "aperçu" du rectangle/lignes en cours de déplacement.
+            this.previewShape();
+        }
+    };
+
+    // Fonction pour dessiner la forme (par exemple, un rectangle)
+    this.drawShape = function() {
+        // Ici on dessine un rectangle avec les coordonnées de départ et d'arrivée
+        var ctx = canvas.getContext('2d');
+        ctx.beginPath();
+        ctx.rect(this.xDebutDND, this.yDebutDND, this.xFinDND - this.xDebutDND, this.yFinDND - this.yDebutDND);
+        ctx.stroke();
+    };
+
+    // Fonction pour prévisualiser la forme en cours de déplacement
+    this.previewShape = function() {
+        // Effacer tout ce qui a été dessiné précédemment sur le canvas
+        var ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height); // Effacer le canvas entier
+
+        // Dessiner à nouveau le rectangle de départ
+        ctx.beginPath();
+        ctx.rect(this.xDebutDND, this.yDebutDND, this.xFinDND - this.xDebutDND, this.yFinDND - this.yDebutDND);
+        ctx.stroke();
+    };
+
+    // Associer les événements du canvas à la fonction 'mouseDown'
+    canvas.addEventListener('mousedown', this.mouseDown.bind(this));
 };
 
-
-// Place le point de l'événement evt relativement à la position du canvas.
+// Fonction pour obtenir la position de la souris relative au canvas
 function getMousePosition(canvas, evt) {
-  var rect = canvas.getBoundingClientRect();
-  return {
-    x: evt.clientX - rect.left,
-    y: evt.clientY - rect.top
-  };
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
 };
-
-
-
--- model.js
+++ model.js
@@ -1,3 +1,28 @@
 
 // Implémenter ici les 4 classes du modèle.
+
 // N'oubliez pas l'héritage !
+
+
+function Drawing (){
+    this.drawing = new Map()
+}
+
+function Forme (startX, startY,couleur , epaisseur){
+this.startX=startX
+this.startY=startY
+this.couleur= couleur
+this.epaisseur= epaisseur
+}
+function Rectangle(startX, startY,couleur , epaisseur,hauteur, largeur){
+Forme.call(startX, startY,couleur , epaisseur)
+this.hauteur=hauteur
+this.largeur= largeur
+
+}
+
+function Line(startX, startY,couleur , epaisseur,endX ,endY){
+    Forme.call(startX, startY,couleur , epaisseur)
+    this.endX=endX
+    this.endY= endY
+}
